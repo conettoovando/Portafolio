decca45a157992d1f1241a3e50b7d58f64bef530
conettoovando <cristian.onetto@hotmail.com>
2023-08-10 00:22:43 -0400

    actualizacion de vista, espaciado, navbar colors, navbar posicion

--- src/app/components/navBar/navBar.component.ts
+++ src/app/components/navBar/navBar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import routesNames, { svgIcons } from 'src/app/Constant/routerNames';
 
 @Component({
@@ -7,6 +7,26 @@ import routesNames, { svgIcons } from 'src/app/Constant/routerNames';
   styleUrls: ['./navBar.component.css']
 })
 export class NavBarComponent {
+  activeSectionId: string | null = null;
+
+  setActiveSection(sectionId: string | null): void {
+    this.activeSectionId = sectionId;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    const sections = ['Inicio', 'Sobre mi', 'Proyectos', 'Contacto', 'footer'];
+    const fromTop = window.scrollY;
+
+    for (const sectionId of sections) {
+      const section = document.getElementById(sectionId);
+      if (section && section.offsetTop <= fromTop + 300 && section.offsetTop + section.offsetHeight > fromTop + 300) {
+        this.setActiveSection(sectionId);
+        break;
+      }
+    }
+  }
+
   public tag: string = "#CAOO";
   public navOptions: string[] = [routesNames.home, routesNames.sobremi, routesNames.proyectos, routesNames.contacto];
   public navName: string = routesNames.home;
