564938492d4f8124b947494750f5ebb3c6335d05
conettoovando <cristian.onetto@hotmail.com>
2023-08-10 04:06:20 -0400

    v0.9

--- src/app/components/form-contact/form-contact.component.ts
+++ src/app/components/form-contact/form-contact.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-contact.component.css']
 })
 export class FormContactComponent {
+  sendMail = (event: Event) => {
+    event.preventDefault();
+
+    const myForm = event.target as HTMLFormElement;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData as any).toString()
+    }).then(() => alert("¡Gracias por tu envío!"))
+      .catch((error) => alert(error));
+  }
 }
