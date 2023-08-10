19b298f98eb66c47b4a4f6cfb1678e1d83c3990f
conettoovando <cristian.onetto@hotmail.com>
2023-08-10 16:03:03 -0400

    v0.12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/navBar/navBar.component';
@@ -9,6 +10,7 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RrssComponent } from './components/rrss/rrss.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { FormContactComponent } from './components/form-contact/form-contact.component';
 
 
 @NgModule({
@@ -21,9 +23,11 @@ import { ContactComponent } from './components/contact/contact.component';
     FooterComponent,
     RrssComponent,
     ContactComponent,
+    FormContactComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/form-contact/form-contact.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Component({
+  selector: 'app-form-contact',
+  templateUrl: './form-contact.component.html',
+  styleUrls: ['./form-contact.component.css']
+})
+export class FormContactComponent {
+
+  constructor(private http: HttpClient) { }
+
+  person = {
+    name: '',
+    email: '',
+  }
+
+  procesar() {
+    const body = new HttpParams().set('form-name', 'contact').append('name', this.person.name).append('email', this.person.email);
+    this.http.post('/', body.toString(), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).subscribe(
+      res => {
+        console.log(res);
+      }
+    );
+  }
+}
